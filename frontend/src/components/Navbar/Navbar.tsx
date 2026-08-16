1f21edcda52aee9e4ed38082162247401af02df9
Akshats23 <akshats2023@gmail.com>
2026-08-16 12:11:42 -0400

    UI updates

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -11,24 +11,26 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function Navbar() {
   return (
-    <AppBar position="static">
-      <Toolbar>
+    <AppBar
+  position="static"
+  elevation={0}
+  sx={{
+    backgroundColor: "#111827",
+    borderRadius: "0 0 20px 20px",
+    px: 2,
+    mb: 3,
+  }}
+>
+      <Toolbar sx={{ minHeight: 64 }}>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           E-Commerce Manager
         </Typography>
 
         <Button color="inherit">Dashboard</Button>
         <Button color="inherit">Products</Button>
-        <Button color="inherit">Orders</Button>
-        <Button color="inherit">Categories</Button>
-        <Button color="inherit">Customers</Button>
-         <Button
-          color="inherit"
-          component={Link}
-          to="/products/new"
-        >
-          Add Product
-        </Button>
+        {/* <Button color="inherit">Orders</Button>
+        <Button color="inherit">Categories</Button> */}
+        {/* <Button color="inherit">Customers</Button> */}
 
         <IconButton color="inherit">
           <NotificationsIcon />
